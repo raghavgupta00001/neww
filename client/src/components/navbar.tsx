7003ae2d0475f22afab12dde12b6d3cb8da7b4a8
Raghav Gupta <39884688-2024kuec2074@users.noreply.replit.com>
2025-02-22 11:50:27 +0000

    Restored to 'fa42d5d48184a61abefbe9b65743d335182b2c16'
    Replit-Restored-To:fa42d5d48184a61abefbe9b65743d335182b2c16

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -12,7 +12,7 @@ export function Navbar() {
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center space-x-4">
             <Link href="/" className="flex items-center">
-              <img src="/logo.png" alt="PlagiCheck" className="h-12" />
+              <img src="/logo.png" alt="PlagiCheck" className="h-8" />
             </Link>
 
             <div className="hidden md:flex space-x-4 ml-8">
